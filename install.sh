8c914010d1d581baa7855e46c4069188b1461ecc
Pawaris Mopha <251288879+PawarisMopha@users.noreply.github.com>
2026-05-21 20:53:27 +0700

    Fix formatting issues in install.sh

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ apt-get update
 apt-get  --assume-yes upgrade
 apt-get  --assume-yes install coreutils gnupg wget
 if [ ! -f "$PREFIX/etc/apt/sources.list.d/termux-adb.list" ]; then
-  mkdir -p $PREFIX/etc/apt/sources.list.d
-  echo -e "deb https://nohajc.github.io termux extras" > $PREFIX/etc/apt/sources.list.d/termux-adb.list
-  wget -qP $PREFIX/etc/apt/trusted.gpg.d https://nohajc.github.io/nohajc.gpg
+  mkdir -p "$PREFIX/etc/apt/sources.list.d"
+  echo -e "deb https://nohajc.github.io termux extras" > "$PREFIX/etc/apt/sources.list.d/termux-adb.list"
+  wget -qP "$PREFIX/etc/apt/trusted.gpg.d https://nohajc.github.io/nohajc.gpg"
   apt update
 else
   echo "Repo already installed"
