be0133103223c831ca5d30e0143369c326b34b46
Pawaris Mopha <pawaris2732@gmail.com>
2026-05-21 21:21:10 +0700

    Initial commit on master branch

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+apt-get update
+apt-get  --assume-yes upgrade
+apt-get  --assume-yes install coreutils gnupg wget
+if [ ! -f "$PREFIX/etc/apt/sources.list.d/termux-adb.list" ]; then
+  mkdir -p "$PREFIX/etc/apt/sources.list.d"
+  echo -e "deb https://nohajc.github.io termux extras" > "$PREFIX/etc/apt/sources.list.d/termux-adb.list"
+  wget -qP "$PREFIX/etc/apt/trusted.gpg.d https://nohajc.github.io/nohajc.gpg"
+  apt update
+else
+  echo "Repo already installed"
+fi
+apt install termux-adb
+
+echo "done!"
